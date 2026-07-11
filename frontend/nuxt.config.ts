335068f739c900b14130ffb72a65d14167772b85
andreyserdyuk-sys <andrey.serdyuk@creativecode.digital>
2026-07-11 10:06:45 +0700

    bug fix

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/main.css'],
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
