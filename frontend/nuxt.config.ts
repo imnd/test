80607f88bcef4c628199f1c4a4316c714027db97
andreyserdyuk-sys <andrey.serdyuk@creativecode.digital>
2026-07-11 11:46:22 +0700

    main.css -> main.scss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/main.css'],
+  css: ['~/assets/main.scss'],
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
     }
   },
+  modules: ['@pinia/nuxt'],
   components: [
     {
       path: '~/components/UI',
